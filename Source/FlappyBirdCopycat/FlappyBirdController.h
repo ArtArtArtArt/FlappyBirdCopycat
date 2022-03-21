4b3bf0d69528543c02b668376b0955039494729c
ArtArtArtArt <artemdymchenko26@gmail.com>
2022-03-21 20:10:28 +0200

    new classes

---
+++ Source/FlappyBirdCopycat/FlappyBirdController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FlappyBirdController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAPPYBIRDCOPYCAT_API AFlappyBirdController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/FlappyBirdCopycat/FloppyBirdCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "FloppyBirdCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAPPYBIRDCOPYCAT_API AFloppyBirdCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
